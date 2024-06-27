8ed39da1c82f36a9d18b2443763a0335798dd710
anknits <ankuragarwall04@gmail.com>
2024-06-27 13:06:46 +0530

    ship static data with app - genres and platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,15 @@
-import useData from "./useData";
+//import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
-    id: number;
-    name: string;
-    slug: string;
-    image_background: string;
-    games_count: number;
+  id: number;
+  name: string;
+  slug: string;
+  image_background: string;
+  games_count: number;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+//const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
