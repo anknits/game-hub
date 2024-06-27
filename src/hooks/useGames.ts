95279a3669905e5b2b9baab84fa82b6ea4b7a972
anknits <ankuragarwall04@gmail.com>
2024-06-27 11:54:52 +0530

    Implementing game search

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,20 +3,25 @@ import useData from "./useData";
 import { Platform } from "./usePlatforms";
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-const useGames = (gameQuery: GameQuery) => 
-    useData<Game>("/games", 
-        {params:{
-            genres: gameQuery.genre?.id, 
-            platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder
-            }}, 
-        [gameQuery]);
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
+      },
+    },
+    [gameQuery]
+  );
 
 export default useGames;
